12460607259b231198a8cd9fdddd73f365b45e67
hparquet <henri.parquet@niji.fr>
2020-01-02 19:47:47 +0100

    better test

--- waitgroup_test.go
+++ waitgroup_test.go
@@ -7,17 +7,37 @@ import (
 )
 
 func TestWaitGroup(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
-	defer cancel()
 
-	var wg Group
+	testSuite := []struct {
+		timeOutDuration, operationDuration time.Duration
+		expectedErr                        error
+	}{
+		{
+			timeOutDuration:   1 * time.Millisecond,
+			operationDuration: 1 * time.Second,
+			expectedErr:       context.DeadlineExceeded,
+		},
+		{
+			timeOutDuration:   1 * time.Second,
+			operationDuration: 1 * time.Millisecond,
+			expectedErr:       nil,
+		},
+	}
 
-	wg.Add(1)
-	go func() {
-		time.Sleep(1 * time.Second)
-	}()
+	for _, test := range testSuite {
+		ctx, cancel := context.WithTimeout(context.Background(), test.timeOutDuration)
+		defer cancel()
 
-	if err := wg.WaitWithContext(ctx); err != context.DeadlineExceeded {
-		t.Fatalf("err should be %v, got %v", context.DeadlineExceeded, err)
+		var wg Group
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(test.operationDuration)
+		}()
+
+		if err := wg.WaitWithContext(ctx); err != test.expectedErr {
+			t.Fatalf("err should be %v, got %v", test.expectedErr, err)
+		}
 	}
 }
